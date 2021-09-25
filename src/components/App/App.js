f4386e9d6a2f40ca33567c7bc5d70e33385e44f0
Andrew TOwnsley <atownsley86@gmail.com>
2021-09-24 21:09:23 -0600

    Tutorial Complete

---
+++ src/components/App/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login';
+import useToken from './useToken';
+
+
+const App = () => {
+    const { token, setToken } = useToken();
+
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
+
+    return (
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/preferences">
+                        <Preferences />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+    </div>
+    )
+}
+
+export default App
+
